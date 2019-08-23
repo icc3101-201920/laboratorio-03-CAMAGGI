3e6c33f1716c0ba3b821826c9c400aac6c0f140d
CAMAGGI <43652021+CAMAGGI@users.noreply.github.com>
2019-08-23 10:30:33 -0400

    asdgf

--- Lab 03 POO Maggi/Lab 03 POO Maggi/Deck.cs
+++ Lab 03 POO Maggi/Lab 03 POO Maggi/Deck.cs
@@ -5,25 +5,29 @@ using System.Text;
 
 namespace Laboratorio_2_OOP_201902
 {
+    namespace Laboratorio_2_OOP_201902
+{
     public class Deck
     {
-        private List<CombatCard> combatCards;
-        private List<SpecialCard> SpecialCards;
+        private List<Card.Card> cards;
+        
 
         public Deck()
         {
 
         }
 
-        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
-        public List<SpecialCard> SpecialCards1 { get => SpecialCards; set => SpecialCards = value; }
-
-        public void AddCombatCard(CombatCard combatCard) { throw new NotImplementedException(); }
-        public void AddSpecialCard(SpecialCard specialCard) { throw new NotImplementedException(); }
-        public void DestroyCombatCard(int cardId) { throw new NotImplementedException(); }
-        public void DestroySpecialCard(int cardId) { throw new NotImplementedException(); }
+        public List<Card.Card> CombatCards { get => cards; set => cards = value; }
+       
+        public void AddCard(Card.Card card) { throw new NotImplementedException(); }
+      
+        public void DestroyCard(int cardId) { throw new NotImplementedException(); }
+    
         public void Shuffle() { 
             throw new NotImplementedException();
         }
+    }
+}
+
     }
 }
